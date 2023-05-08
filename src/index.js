4010a0139b48a0ed7e4a6d8b719713bc0a3e7164
Alejandro Castaneda <alejandroxd62@gmail.com>
2023-05-08 17:30:37 -0600

    creación de una ruta

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require ('express')
+const router = require('./routes') //solo si se llama index
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get ('/', (req, res) => {
-    res.send("Holi");
-})
+// app.get ('/', (req, res) => {
+//     res.send("Holi");
+// })
+
+app.use("/api", router)
 
 app.listen (PORT, () => {
     console.log(`Servidor escuchando en puerto ${PORT} siuuu 🖥️`);
