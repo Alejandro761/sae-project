d6f1aeca5efb68b3fadc66b4269988f50bc9ec12
Alejandro Castaneda <alejandroxd62@gmail.com>
2023-05-09 09:41:08 -0600

    añadi las opciones de busqueda y filtros, también edite resources de routes para que que cada link contenga el area

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -3,23 +3,38 @@ import React, {useState} from 'react'
 export const Search = () => {
     const api = 'http://localhost:3001/api/resources/'
     const [text, setText] = useState(''); 
+    const [option, setOption] = useState(''); 
+    const [area, setArea] = useState('todas'); 
 
-    const getAllResourcesByType = async (type, auth) => {
-        const response = await fetch (`${api}${type}/${auth}`);
+    const getAllResourcesByType = async (option, params) => {
+        const response = await fetch (`${api}${option}/${params}`);
         const data = await response.json();
         console.log(data)
     }
-    const searching = (valueText) => {
-        getAllResourcesByType("title",text)
+
+    const searching = () => {
+        getAllResourcesByType(option,text)
     }
 
     return (
         <div>
             <form>
+                <select name='Seleccione un filtro para su busqueda' onChange={e => setOption(e.target.value)}>
+                    <option value={'author'}>Autor</option>
+                    <option value={'editorial'}>Editorial o canal</option>
+                    <option value={'title'}>Titulo del recurso</option>
+                </select>
                 <input type="text" name="" id="text" onChange={e => setText(e.target.value)}/>
+                <select name='Selecciona un área de estudio' onChange={e => setArea(e.target.value)}>
+                    <option value = {'economico'}>Económico administrativa</option>
+                    <option value={'ingenieria'}>Ingeniería y ciencias exactas</option>
+                    <option value={'medicina'}>Ciencias naturales y de la salud</option>
+                    <option value={'sociales'}>Ciencias sociales y humanidades</option>
+                    <option value={'todas'}>Cualquiera</option>
+                </select>
             </form>
             <h1>text = {text}</h1>
-            <button onClick={e => searching(text)}>Buscar</button>
+            <button onClick={e => searching()}>Buscar</button>
         </div>
     )
 }
--- src/routes/resources.js
+++ src/routes/resources.js
@@ -4,9 +4,9 @@ const resourcesController = require('../controllers/resources')
 
 router 
     .get ("/", resourcesController.getAllResources)
-    .get ("/author/:author", resourcesController.getResourcesByAuthor)
-    .get ("/area/:area", resourcesController.getResourcesByArea)
-    .get ("/editorial/:editorial", resourcesController.getResourcesByEditorial)
-    .get ("/title/:title", resourcesController.getResourcesByTitle)
+    .get ("/author/:author/area/:area", resourcesController.getResourcesByAuthor)
+    // .get ("/area/:area/area/:area", resourcesController.getResourcesByArea)
+    .get ("/editorial/:editorial/area/:area", resourcesController.getResourcesByEditorial)
+    .get ("/title/:title/area/:area", resourcesController.getResourcesByTitle)
 
 module.exports = router
