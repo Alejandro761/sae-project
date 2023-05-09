7bc680451b4466d8d5e7ca55ad6f762e854d4676
Alejandro Castaneda <alejandroxd62@gmail.com>
2023-05-09 02:15:44 -0600

    creacion de componente search

---
+++ frontend/src/components/Search.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Search = () => {
+    return (
+        <form>
+            <input type="text" name="" id=""  value="Libros, autores, editoriales, temas, etcétera"/>
+            <input type="button" value="Buscar"/>
+        </form>
+    )
+}
