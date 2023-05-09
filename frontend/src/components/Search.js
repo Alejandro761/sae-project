a58e1e3d6c42af3be2a2316f8de7d92231324b6f
Alejandro Castaneda <alejandroxd62@gmail.com>
2023-05-09 03:13:21 -0600

    boton buscar ya lee el input y realiza la llamada ala api, falta el input del lista de tipos filtros

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const Search = () => {
+    const api = 'http://localhost:3001/api/resources/'
+    const [text, setText] = useState(''); 
+
+    const getAllResourcesByType = async (type, auth) => {
+        const response = await fetch (`${api}${type}/${auth}`);
+        const data = await response.json();
+        console.log(data)
+    }
+    const searching = (valueText) => {
+        getAllResourcesByType("title",text)
+    }
+
     return (
-        <form>
-            <input type="text" name="" id=""  value="Libros, autores, editoriales, temas, etcétera"/>
-            <input type="button" value="Buscar"/>
-        </form>
+        <div>
+            <form>
+                <input type="text" name="" id="text" onChange={e => setText(e.target.value)}/>
+            </form>
+            <h1>text = {text}</h1>
+            <button onClick={e => searching(text)}>Buscar</button>
+        </div>
     )
 }
